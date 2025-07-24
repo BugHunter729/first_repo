47b504df181364fb377425bf2778dafbcbe8d3ac
BugHunter729 <kavyarajcusat@gmail.com>
2025-07-24 15:38:44 +0000

    various changes and newfiles

---
+++ 01_basics/03_conv_op.js
@@ -0,0 +1,50 @@
+let score = true
+console.log(typeof score);
+console.log(typeof(score))
+let valueNumber = Number(score)
+console.log(typeof valueNumber)
+console.log(valueNumber)
+//"33"=>33
+//"33abc"=>NAN
+//true=>1; false=>0;
+//null=>0
+//undefined=>NAN
+let isLoggedIn = "kavya";
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1=>true , 0=>false
+//"hitesh" =>true
+let someNumber =  "avevwer" 
+let stringNUmber = String(someNumber)
+console.log(typeof(stringNUmber))
+
+
+// ****************Operations********//
+let value  = 3;
+let negvalue = -value
+console.log(negvalue)
+console.log(2**3)
+console.log(2/3)
+console.log(2%3)
+let str1 = "hello"
+let str2 = "kavya"
+let str3= str2+str1
+console.log(str3)
+console.log("1"+2)
+console.log(1+"2")
+console.log(1+2+"2")
+console.log(1+"3"+4)
+console.log("1"+2+5)
+console.log(+true)
+// console.log(true+) will throw error
+// console.log(++true) will throw error
+console.log(+"") // gives 0 as output
+let num1,num2,num3
+num1=num2=num3= 2+2
+console.table([num1,num2,num3])
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
+
+
+
---
+++ 01_basics/04_comp.js
@@ -0,0 +1,9 @@
+console.log(2==2);
+console.log("02">1)
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+console.log(undefined<0);
+//=== strictly check for equality including datatype
+console.log("2"===2)
+// the conversion from line 3 to 8 can make user confuse because of the inconsistency in javascript language
